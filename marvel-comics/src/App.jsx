f33ddb5d97e8ae90cd17e907a010d2b4620632e8
Mari300 <jahfly9@gmail.com>
2025-01-07 18:15:08 -0500

    changed the package.json

--- marvel-comics/src/App.jsx
+++ marvel-comics/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   const [error, setError] = useState('');
 
   return (
-    <BrowserRouter basename="/marvel-comic-react-project">
+    <BrowserRouter>
       <CharacterContext.Provider value={{ characters, error, setCharacters, setError }}>
 
         <ComicContext.Provider value={{ comics, error, setComics, setError }}>
