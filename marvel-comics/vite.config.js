a2a2d67e931af2af5060accd1db86d1bc5c8f07c
Mari300 <jahfly9@gmail.com>
2025-01-09 21:11:05 -0500

    changed config base back to previous

--- marvel-comics/vite.config.js
+++ marvel-comics/vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig(({ mode }) => {
       'process.env': env
     },
     plugins: [react()],
-    base: 'https://react-api-project.github.io/',
+    base: '/marvel-comic-react-project/',
   }
 })
